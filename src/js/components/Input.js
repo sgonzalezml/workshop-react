ed00f5589e1e834eac269aa68501159edd73366f
Sebastian <sebastian.sgonzalez@mercadolibre.com>
2018-01-09 15:31:18 -0200

    remove aria attributes

--- src/js/components/Input.js
+++ src/js/components/Input.js
@@ -6,17 +6,17 @@ export default class Input extends Component {
         return (<div className="input">
             <div>
                 <div className="input-group mb-3">
-                    <input type="text" placeholder="Titulo" className="form-control input-title" aria-label="Title" aria-describedby="inputGroup-sizing-default"/>
+                    <input type="text" placeholder="Titulo" className="form-control input-title" />
                 </div>
             </div>
             <div>
                 <div className="input-group mb-3">
-                    <input type="text" placeholder="Autor" className="form-control input-author" aria-label="Author" aria-describedby="inputGroup-sizing-default"/>
+                    <input type="text" placeholder="Autor" className="form-control input-author" />
                 </div>
             </div>
             <div>
                 <div className="input-group mb-3">
-                    <input type="textarea" placeholder="Descripcion" className="form-control input-desc" aria-label="Descripcion" aria-describedby="inputGroup-sizing-default"/>
+                    <input type="textarea" placeholder="Descripcion" className="form-control input-desc" />
                 </div>
             </div>
             <button type="button" class="btn btn-secondary">Post!</button>
